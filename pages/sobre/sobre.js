af5cf0a06b7ad64bbe4329ba7d3c73cba460ca8e
AmandaLFreitas <freitasamandalais@gmail.com>
2024-11-10 22:39:51 -0300

    feat: sobre

---
+++ pages/sobre/sobre.js
@@ -0,0 +1,51 @@
+// Comente a linha a seguir se optar por um logger mais sofisticado e adicione o script de logger
+// const logger = false;
+const APP_NAME = 'Segurança Alimentar';
+const breadcrumbPath = (title) => {
+  return [
+    {
+      label: 'Início',
+      path: '../index.html',
+    },
+    {
+      label: title,
+      path: '#',
+    },
+  ];
+};
+const sections = [
+  {
+    path: 'about',
+    label: 'Sobre',
+  },
+];
+
+// No momento é fixado o carregamento de dados da página, no qual a estrutura
+// é fixa, mudando apenas os dados, mas o correto seria avaliar
+// se há necessidade de algum render em específico, baseado nos dados carregados
+const render = {
+  body: bodyRender,
+};
+
+function run(key) {
+  const [isLoading, setIsLoading] = loading();
+  updateDocumentTitle(APP_NAME);
+  const [isOpen, setIsOpen] = sidebar(key, MENU_ITEMS.main, MENU_ITEMS.default);
+  toolbar(APP_NAME, () => setIsOpen(true));
+  setIsLoading(true);
+
+  window.addEventListener('load', async () => {
+    const pageData = await fetchData(key, ({ title, icon }) => {
+      breadcrumb(breadcrumbPath(title));
+      updateDocumentTitle(`${APP_NAME} | ${title}`);
+      mainRender(title, [], sections);
+      footer(APP_NAME);
+    });
+
+    await pageRender(pageData, render, () => {
+      setIsLoading(false);
+    });
+  });
+}
+
+run(pageKey.SOBRE);
